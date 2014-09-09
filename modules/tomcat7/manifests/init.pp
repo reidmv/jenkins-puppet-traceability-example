b5fffa9039306a56fcd15f4ef1bb5f2d3406316f
Reid Vandewiele <reid@puppetlabs.com>
2014-09-09 07:46:24 -0700

    Style guide adjustments for puppet manifests
    This commit makes non-functional style changes to the puppet manifests
    in order to align them with the published style guide
    (https://docs.puppetlabs.com/guides/style_guide.html).

--- modules/tomcat7/manifests/init.pp
+++ modules/tomcat7/manifests/init.pp
@@ -1,49 +1,52 @@
 # manage filetest
 class tomcat7 {
 
-      package { 'wget':
-      	      ensure => installed,
-      }
+  package { 'wget':
+    ensure => installed,
+  }
 
-      exec { 'apt-get update':
-      	   command => 'apt-get update',
-	   path => ['/usr/bin'],
-      	   
-      }
-      package { 'openjdk-7-jdk':
-      	      ensure => installed, 
-	      require => Exec['apt-get update'],
-      }
+  exec { 'apt-get update':
+    command => 'apt-get update',
+    path    => ['/usr/bin'],
+  }
 
-      package { 'tomcat7':
-      	      ensure => installed, 
-      	      require => Package['openjdk-7-jdk'],
-      }
-      service { 'tomcat7':
-      	      ensure => running, 
-	      require => Package['tomcat7'],
-      }
+  package { 'openjdk-7-jdk':
+    ensure  => installed,
+    require => Exec['apt-get update'],
+  }
 
-      exec { 'sample-war':
-      	      require => [ 
-	      	      Service['tomcat7'], 
-		      Package['wget']
-	      ],
-	      command => 'wget http://10.0.2.2:8080/job/PuppetJavaSample/lastSuccessfulBuild/artifact/target/traceability.war -P /tmp/',
-	      path => ['/usr/bin'],
-      }
+  package { 'tomcat7':
+    ensure  => installed,
+    require => Package['openjdk-7-jdk'],
+  }
 
-      exec { 'delete-war-from-webapps':
-	      command => 'rm -rf /var/lib/tomcat7/webapps/traceability*',
-	      path => ['/bin'],
-      }
+  service { 'tomcat7':
+    ensure  => running,
+    require => Package['tomcat7'],
+  }
 
-      exec { 'delete-war-from-tmp':
-      	   command => 'rm /tmp/traceability.war',
-	   path => ['/bin'],
-      }
-      file { '/var/lib/tomcat7/webapps/traceability.war':
-      	   source => '/tmp/traceability.war',
-	   require => Exec['sample-war', 'delete-war-from-webapps'],
-      }
+  exec { 'sample-war':
+    command => 'wget http://10.0.2.2:8080/job/PuppetJavaSample/lastSuccessfulBuild/artifact/target/traceability.war -P /tmp/',
+    path    => ['/usr/bin'],
+    require => [
+      Service['tomcat7'],
+      Package['wget']
+    ],
+  }
+
+  exec { 'delete-war-from-webapps':
+    command => 'rm -rf /var/lib/tomcat7/webapps/traceability*',
+    path    => ['/bin'],
+  }
+
+  exec { 'delete-war-from-tmp':
+    command => 'rm /tmp/traceability.war',
+    path    => ['/bin'],
+  }
+
+  file { '/var/lib/tomcat7/webapps/traceability.war':
+    source  => '/tmp/traceability.war',
+    require => Exec['sample-war', 'delete-war-from-webapps'],
+  }
+
 }
